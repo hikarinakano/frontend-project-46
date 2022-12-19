03f394b2ba108cdae5626febc70dfab4f8f49cc2
Irina <hikarinakano95@gmail.com>
2022-12-19 18:42:39 +0100

    Added bin/gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
+
+program.parse();
+
+
+
