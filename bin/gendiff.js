a98fd1a755aa9b07277eeebfec82eeef583830e7
Irina <hikarinakano95@gmail.com>
2022-12-20 16:44:06 +0100

    Added new option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,10 @@ const program = new Command();
 program
 
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('filepath1')
+  .argument('filepath2');
 
 program.parse();
 
