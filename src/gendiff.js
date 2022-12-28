8d4942c6b6fd6f64890b86369d6b20964a18c7c9
Irina <hikarinakano95@gmail.com>
2022-12-28 15:38:53 +0100

    Added src folder with gendiff.js

---
+++ src/gendiff.js
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+
+
+export default (filepath1, filepath2) => {
+  const data1 = readFileSync(filepath1, 'utf-8');
+  const data2 = readFileSync(filepath2, 'utf-8');
+
+  const parsedData1 = JSON.parse(data1);
+
+  const parsedData2 = JSON.parse(data2);
+  return genDiff(parsedData1, parsedData2);
+};
+
+const genDiff = (parsedData1, parsedData2) => {
+  const keys = Object.keys({ ...parsedData1, ...parsedData2 }).sort(); // https://youtu.be/vkUTX1hruF8?t=929
+  const result = ['{'];
+  for (const key of keys) {
+    if (!Object.hasOwn(parsedData1, key)) {
+      result.push(`  + ${key}: ${parsedData2[key]}`);
+    } else if (!Object.hasOwn(parsedData2, key)) {
+      result.push(`  - ${key}: ${parsedData1[key]}`);
+    } else if (parsedData1[key] !== parsedData2[key]) {
+      result.push(`  - ${key}: ${parsedData1[key]}`);
+      result.push(`  + ${key}: ${parsedData2[key]}`);
+    } else {
+      result.push(`    ${key}: ${parsedData2[key]}`);
+    }
+  }
+  result.push('}');
+  return result.join('\n');
+};
