cbf664ae65b47edbb7d10b75067290a21b410103
Irina <hikarinakano95@gmail.com>
2022-12-28 15:34:59 +0100

    Added main file in bin directory

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/gendiff.js';
 const program = new Command();
 
 program
-
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('filepath1')
-  .argument('filepath2');
+  .argument('<filepath1>', 'path to file1')
+  .argument('<filepath2>', 'path to file2')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
-program.parse();
+program.parse(process.argv);
 
 
 
