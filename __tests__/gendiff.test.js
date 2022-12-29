d2462be9e5241f03b603ecb730be9a555fc13050
Irina <hikarinakano95@gmail.com>
2022-12-29 18:04:08 +0100

    fixed tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,9 +1,9 @@
-import { test, expect, beforeEach } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import gendiff from '../src/gendiff';
 
-  
+
 
 test('primary test', () => {
   const getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__', filename);
@@ -12,10 +12,10 @@ test('primary test', () => {
   const json2 = readFile('file2.json');
   const data1 = getFixturePath('file1.json');
   const data2 = getFixturePath('file2.json');
-  
+
   expect(Object.keys(json1).length).not.toBe(0);
   expect(Object.keys(json2).length).not.toBe(0);
   expect(json1).not.toEqual(json2);
   expect(gendiff(data1, data2)).not.toEqual('{}')
-  expect(typeof(gendiff(data1, data2))).toEqual('string')
+  expect(typeof (gendiff(data1, data2))).toEqual('string')
 })
