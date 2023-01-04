52526da44a33264fa65add065042e1794c5db42d
Irina <hikarinakano95@gmail.com>
2023-01-04 18:47:33 +0100

    Added small changes to implement later

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,18 +1,32 @@
-import { readFileSync } from 'fs';
+
+//import parser from 'src/parser.js';
+
+//parser(filepath1, filepath2);
+
+import yaml from 'js-yaml';
+import fs from 'fs';
 
 
 export default (filepath1, filepath2) => {
-  const data1 = readFileSync(filepath1, 'utf-8');
-  const data2 = readFileSync(filepath2, 'utf-8');
-
-  const parsedData1 = JSON.parse(data1);
-  const parsedData2 = JSON.parse(data2);
+  const data1 = fs.readFileSync(filepath1, 'utf-8');
+  const data2 = fs.readFileSync(filepath2, 'utf-8');
+  const extension = data1.split('.')[1];
+  let parsedData1, parsedData2;
+  if (extension === 'yml' || extension === 'yaml') {
+    parsedData1 = yaml.load(data1, 'utf8');
+    parsedData2 = yaml.load(data2, 'utf8');
+  }
+  else if (extension === 'json') {
+    parsedData1 = JSON.parse(data1);
+    parsedData2 = JSON.parse(data2);
+  }
+  console.log(yaml.load(data1, 'utf8'))
   return genDiff(parsedData1, parsedData2);
-};
+}
 
-const genDiff = (parsedData1, parsedData2) => {
+function genDiff(parsedData1, parsedData2) {
   const keys = Object.keys({ ...parsedData1, ...parsedData2 }).sort(); // https://youtu.be/vkUTX1hruF8?t=929
-  const result = ['{'];
+  const result = [];
   for (const key of keys) {
     if (!Object.hasOwn(parsedData1, key)) {
       result.push(`  + ${key}: ${parsedData2[key]}`);
@@ -25,6 +39,5 @@ const genDiff = (parsedData1, parsedData2) => {
       result.push(`    ${key}: ${parsedData2[key]}`);
     }
   }
-  result.push('}');
   return result.join('\n');
 };
