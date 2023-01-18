abb2c0b6fd3f1824c562d5f91881b22a0b566fb9
Irina <hikarinakano95@gmail.com>
2023-01-18 20:01:32 +0100

    Moved stylish to another module, parted it from genDiff

---
+++ src/stylish.js
@@ -0,0 +1,29 @@
+
+function stylish(data) {
+  return doStylish(data).join('');
+}
+
+function doStylish(data, indent = '') {
+  const newIndent = `${indent}    `;
+  let result = ['{\n'];
+  for (const elem of data) {
+    let key = elem.key
+    if (elem['status'] === 'unchanged') {
+      result.push(`${indent}    ${key}: `)
+    }
+    else if (elem['status'] === 'added') {
+      result.push(`${indent}  + ${key}: `)
+    }
+    else if (elem['status'] === 'deleted') {
+      result.push(`${indent}  - ${key}: `)
+    }
+    if (Array.isArray(elem['value'])) {
+      result = result.concat(doStylish(elem['value'], newIndent))
+    }
+    else { result.push(`${elem['value']}\n`) }
+  }
+  result.push(`${indent}}\n`);
+  return result;
+}
+
+export default stylish;
