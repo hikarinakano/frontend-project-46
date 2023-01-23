fd8ce273863de073a3ce8b022e3415a89f062c7b
Irina <hikarinakano95@gmail.com>
2023-01-23 22:24:57 +0100

    Fixed gendiff tests" \

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,20 +2,20 @@ import { test, expect, beforeAll } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
-import parse from '../src/parse';
 
 
-let getFixturePath, readFile, readParsedFile, data1, data2;
+
+let getFixturePath, readFile, filepath1, filepath2;
 beforeAll(() => {
-  getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__', filename);
+  getFixturePath = (filename) => path.join('__fixtures__', filename);
   readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-  readParsedFile = (filename) => parse(readFile(filename), filename);
+
 })
 
 test('genDiff test', () => {
-  data1 = readParsedFile('file1.json');
-  data2 = readParsedFile('file2.json');
-  const result = readFile('gendiff_result.json');
 
-  expect(genDiff(data1, data2)).toEqual(JSON.parse(result))
+  filepath1 = getFixturePath('file1.json')
+  filepath2 = getFixturePath('file2.json');
+  const result = readFile('formatted.txt');
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
 })
