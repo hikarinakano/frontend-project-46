76333f593974b7633bb5a8bad018e65951fe2425
Irina <hikarinakano95@gmail.com>
2023-01-26 15:32:00 +0100

    cleaned up for hexlet-linter check

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,21 +1,14 @@
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
 
-
-
-let getFixturePath, readFile, filepath1, filepath2;
-beforeAll(() => {
-  getFixturePath = (filename) => path.join('__fixtures__', filename);
-  readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-
-})
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiff test', () => {
-
-  filepath1 = getFixturePath('file1.json')
-  filepath2 = getFixturePath('file2.json');
+  const filepath1 = getFixturePath('file1.json')
+  const filepath2 = getFixturePath('file2.json');
   const result = readFile('formatted.txt');
   expect(genDiff(filepath1, filepath2)).toEqual(result);
-})
+});
