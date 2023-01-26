b66a9737d9ce5126fb36e25718c5014892e0ac1e
Irina <hikarinakano95@gmail.com>
2023-01-26 15:33:31 +0100

    cleaned up for hexlet-linter check

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,7 +7,7 @@ const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiff test', () => {
-  const filepath1 = getFixturePath('file1.json')
+  const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const result = readFile('formatted.txt');
   expect(genDiff(filepath1, filepath2)).toEqual(result);
