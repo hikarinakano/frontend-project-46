799229aaade6ba8ee63cf47b64e078c8eb71f5f8
Irina <hikarinakano95@gmail.com>
2023-01-26 15:35:04 +0100

    cleaned up for hexlet-linter check

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -1,29 +1,21 @@
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import parse from '../src/parse';
 
-let getFixturePath, readFile, data1;
-beforeAll(() => {
-  getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__', filename);
-  readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-})
+const getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('json file test', () => {
-
-  data1 = readFile('file1.json');
-
+  const data1 = readFile('file1.json');
   expect(Object.keys(parse(data1, 'file1.json')).length).not.toBe(0);
-})
-
+});
 
 test('yaml file test', () => {
-  data1 = readFile('file1.yml');
-
+  const data1 = readFile('file1.yml');
   expect(Object.keys(parse(data1, 'file1.yml')).length).not.toBe(0);
-})
-
+});
 
 test('wrong path', () => {
-  expect(parse('fileData', 'wrong path')).toEqual('Unsupported format of the file')
-})
+  expect(parse('fileData', 'wrong path')).toEqual('Unsupported format of the file');
+});
