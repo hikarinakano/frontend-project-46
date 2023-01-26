3ae9f3b1d218bed582df733e4122ef8b1650acd7
Irina <hikarinakano95@gmail.com>
2023-01-26 15:30:33 +0100

    cleaned up for hexlet-linter check

--- __tests__/formatters.test.js
+++ __tests__/formatters.test.js
@@ -1,4 +1,4 @@
-import { test, expect, beforeAll } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/gendiff';
