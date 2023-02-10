b9863363f1bbcd7246172cb5b369d28cb9394b47
Irina <hikarinakano95@gmail.com>
2023-02-10 22:08:05 +0100

    Refactored gendiff code to pass hexlet-linter checks

--- src/gendiff.js
+++ src/gendiff.js
@@ -7,37 +7,38 @@ function computeDiff(data1, data2) {
   if (!(data2 instanceof Object)) {
     return data2;
   }
-  function doComputeDiff(keys, data1, data2) {
-    if (keys.length === 0) {
-      return [];
-    }
-    const key = _.first(keys);
-    const rest = _.tail(keys);
-    const restDiffComputed = doComputeDiff(rest, data1, data2);
-    function areEqual(val1, val2) {
-      return val1 === val2;
-    }
-    function areObj(val1, val2) {
-      return val1 instanceof Object && val2 instanceof Object;
-    }
-    function haveMutualKey(data1,data2, key) {
-      return _.has(data1, key) && _.has(data2, key);
-    }
-    if (haveMutualKey(data1, data2, key) && (areEqual(data1[key],data2[key]) || areObj(data1[key],data2[key]))) {
-      const obj = { status: 'unchanged', key: `${key}`, value: computeDiff(data1[key], data2[key]) };
-      return [obj].concat(restDiffComputed);
-    }
-    const result = [
-      _.has(data1, key) && { status: 'deleted', key: `${key}`, value: computeDiff(data1[key], data1[key]) },
-      _.has(data2, key) && { status: 'added', key: `${key}`, value: computeDiff(data2[key], data2[key]) },
-    ];
-    return _.filter(result, (item) => item instanceof Object).concat(restDiffComputed);
-  }
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
   const unsortedKeys = _.union(keys1, keys2);
   const keys = _.sortBy(unsortedKeys);
   return doComputeDiff(keys, data1, data2);
+}
+
+function doComputeDiff(keys, data1, data2) {
+  if (keys.length === 0) {
+    return [];
+  }
+  const key = _.first(keys);
+  const rest = _.tail(keys);
+  const restDiffComputed = doComputeDiff(rest, data1, data2);
+  function areEqual(val1, val2) {
+    return val1 === val2;
+  }
+  function areObj(val1, val2) {
+    return val1 instanceof Object && val2 instanceof Object;
+  }
+  function haveMutualKey(data1, data2, key) {
+    return _.has(data1, key) && _.has(data2, key);
+  }
+  if (haveMutualKey(data1, data2, key) && (areEqual(data1[key], data2[key]) || areObj(data1[key], data2[key]))) {
+    const obj = { status: 'unchanged', key: `${key}`, value: computeDiff(data1[key], data2[key]) };
+    return [obj].concat(restDiffComputed);
+  }
+  const result = [
+    _.has(data1, key) && { status: 'deleted', key: `${key}`, value: computeDiff(data1[key], data1[key]) },
+    _.has(data2, key) && { status: 'added', key: `${key}`, value: computeDiff(data2[key], data2[key]) },
+  ];
+  return _.filter(result, (item) => item instanceof Object).concat(restDiffComputed);
 }
 
 function genDiff(filepath1, filepath2, formatName = 'stylish') {
