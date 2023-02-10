19a3aa9b7bc692839465e88ad341453d3d816f6b
Irina <hikarinakano95@gmail.com>
2023-02-10 12:05:52 +0100

    Code refactored in gendiff to check if passed hexlet-check

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,16 +3,6 @@ import fs from 'fs';
 import parse from './parse.js';
 import getFormatFunction from '../formatters/index.js';
 
-function genDiff(filepath1, filepath2, formatName = 'stylish') {
-  const readFile1 = fs.readFileSync(filepath1, 'utf-8');
-  const readFile2 = fs.readFileSync(filepath2, 'utf-8');
-  const data1 = parse(readFile1, filepath1);
-  const data2 = parse(readFile2, filepath2);
-  const diff = computeDiff(data1, data2);
-  const formatter = getFormatFunction(formatName);
-  return formatter(diff);
-}
-
 function computeDiff(data1, data2) {
   if (!(data2 instanceof Object)) {
     return data2;
@@ -25,7 +15,7 @@ function computeDiff(data1, data2) {
   for (const key of keys) {
     const isVal1Object = data1[key] instanceof Object;
     const isVal2Object = data2[key] instanceof Object;
-    if (_.has(data1, key) && _.has(data2, key) && (data1[key] === data2[key] || isVal1Object && isVal2Object)) {
+    if ((_.has(data1, key) && _.has(data2, key)) && ((data1[key] === data2[key] || isVal1Object && isVal2Object))) {
       result.push({ status: 'unchanged', key: `${key}`, value: computeDiff(data1[key], data2[key]) });
       continue;
     }
@@ -38,4 +28,15 @@ function computeDiff(data1, data2) {
   }
   return result;
 }
+
+function genDiff(filepath1, filepath2, formatName = 'stylish') {
+  const readFile1 = fs.readFileSync(filepath1, 'utf-8');
+  const readFile2 = fs.readFileSync(filepath2, 'utf-8');
+  const data1 = parse(readFile1, filepath1);
+  const data2 = parse(readFile2, filepath2);
+  const diff = computeDiff(data1, data2);
+  const formatter = getFormatFunction(formatName);
+  return formatter(diff);
+}
+
 export default genDiff;
