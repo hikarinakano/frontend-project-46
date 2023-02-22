b5e1deb6a63f5b2305781956fd3fe79974dc58c4
Irina <hikarinakano95@gmail.com>
2023-02-22 11:46:47 +0100

    rewrote tests with tests.each

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -1,22 +1,19 @@
 import { test, expect } from '@jest/globals';
-import fs from 'fs';
+import fs, { read } from 'fs';
 import path from 'path';
 import parse from '../src/parse';
 
 const getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('json file test', () => {
-  const data1 = readFile('file1.json');
-  expect(Object.keys(parse(data1, 'file1.json')).length).not.toBe(0);
-});
-
-test('yaml file test', () => {
-  const data1 = readFile('file1.yml');
-  expect(Object.keys(parse(data1, 'file1.yml')).length).not.toBe(0);
-});
+test.each([
+  { data: readFile('file1.json'), dataSource: 'file1.json' },
+  { data: readFile('file1.yml'), dataSource: 'file1.yml' },
+  ])('parse functioning test', ({data, dataSource}) => {
+    expect(Object.keys(parse(data, dataSource)).length).not.toBe(0);
+  });
 
 test('wrong path', () => {
-  const data1 = readFile('file1.yml');
-  expect(() => { parse(data1, '/some/wrong/path/file.lol'); }).toThrow(Error);
+  const data = readFile('file1.yml');
+  expect(() => { parse(data, '/some/wrong/path/file.lol'); }).toThrow(Error);
 });
