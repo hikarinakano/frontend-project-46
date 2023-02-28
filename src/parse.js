010333c6302c8d0032cb4e1888a0c4d13b872c48
Irina <hikarinakano95@gmail.com>
2023-02-28 20:18:21 +0100

    changed extension to type to pass in function parse

--- src/parse.js
+++ src/parse.js
@@ -1,16 +1,14 @@
 import yaml from 'js-yaml';
-import path from 'path';
 
-function parse(rawData, dataSource) {
-  const extension = path.extname(dataSource).substring(1);
-  switch (extension) {
+function parse(rawData, type) {
+  switch (type) {
     case 'json':
       return JSON.parse(rawData);
     case 'yaml':
     case 'yml':
       return yaml.load(rawData, 'utf8');
     default:
-      throw new Error(`Unsupported format: ${extension}!`);
+      throw new Error(`Unsupported format: ${type}!`);
   }
 }
 export default parse;
