c460d09f7cbdd61865b7b90514d5afcfee7e83eb
Irina <hikarinakano95@gmail.com>
2023-02-28 20:56:59 +0100

    changed tests to pass with type, not format

--- __tests__/parse.test.js
+++ __tests__/parse.test.js
@@ -7,8 +7,8 @@ const getFixturePath = (filename) => path.join('__tests__', '..', '__fixtures__'
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
-  { data: readFile('file1.json'), dataSource: 'file1.json' },
-  { data: readFile('file1.yml'), dataSource: 'file1.yml' },
+  { data: readFile('file1.json'), dataSource: 'json' },
+  { data: readFile('file1.yml'), dataSource: 'yml' },
 ])('parse functioning test', ({ data, dataSource }) => {
   expect(Object.keys(parse(data, dataSource)).length).not.toBe(0);
 });
