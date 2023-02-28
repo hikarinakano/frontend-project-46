dc8c2dfb9a34c6e7b71585d215faeb42cad865bc
Irina <hikarinakano95@gmail.com>
2023-02-28 20:57:53 +0100

    Changed getFormatFunc to format to pass data directly to it

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,17 +2,17 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import jsonFormat from './json.js';
 
-function getFormatFunction(formatName) {
+function format(formatName, data) {
   switch (formatName) {
     case 'plain':
-      return plain;
+      return plain(data);
     case 'json':
-      return jsonFormat;
+      return jsonFormat(data);
     case 'stylish':
-      return stylish;
+      return stylish(data);
     default:
       throw new Error(`Invalid format! There is no format under ${formatName} name!`);
   }
 }
 
-export default getFormatFunction;
+export default format;
