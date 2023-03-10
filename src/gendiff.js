94767e8173df563668f5862ba2e567655e5f794f
Irina <hikarinakano95@gmail.com>
2023-03-10 18:28:46 +0100

    Changed logic to create structure with status changed

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,70 +1,8 @@
-import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parse.js';
 import format from './formatters/index.js';
-
-/*function computeDiff(data1, data2) {
-  if (!(data2 instanceof Object)) {
-    return data2;
-  }
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const unsortedKeys = _.union(keys1, keys2);
-  const keys = _.sortBy(unsortedKeys);
-  return _.map(keys, (key) => {
-    const areEqual = data1[key] === data2[key];
-    const areObj = data1[key] instanceof Object && data2[key] instanceof Object;
-    const haveMutualKey = _.has(data1, key) && _.has(data2, key);
-    if (haveMutualKey && (areEqual || areObj)) {
-      const obj = { status: 'unchanged', key, value: computeDiff(data1[key], data2[key]) };
-      return obj;
-    }
-    const deleted = _.has(data1, key) && { status: 'deleted', key, value: computeDiff(data1[key], data1[key]) };
-    const added = _.has(data2, key) && { status: 'added', key, value: computeDiff(data2[key], data2[key]) };
-    if (deleted && added) {
-      return [deleted, added];
-    }
-    if (deleted) {
-      return deleted;
-    }
-    return added;
-  });
-}*/
-
-function isObj(structure) {
- return structure instanceof Object
-}
-
-function computeDiff(data1, data2) {
-  if (!(data2 instanceof Object)) {
-    return data2;
-  }
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
-  return _.map(keys, (key) => {
-    if (!_.has(data1, key)) {
-      const added = { status: 'added', key, value: computeDiff(data2[key], data2[key]) };
-      return added;
-    }
-    if (!_.has(data2, key)) {
-      return { status: 'deleted', key, value: computeDiff(data1[key], data1[key]) };
-    }
-    if (data1[key] !== data2[key]) {
-      if (isObj(data1[key]) && !isObj(data2[key])) {
-      return { status: 'changed', key, value1: computeDiff(data1[key], data1[key]), value2: data2[key]};
-      }  
-      if (!isObj(data1[key])&& isObj(data2[key])) {
-        return { status: 'changed', key, value1: data1[key], value2: computeDiff(data2[key], data2[key])};
-      }
-      else if (!isObj(data1[key])&& !isObj(data2[key])) {
-        return {status: 'changed', key, value1: data1[key], value2: data2[key]};
-      }
-    }  
-      return { status: 'unchanged', key, value: computeDiff(data1[key], data2[key])};
-  });
-}
+import computeDiff from './computeDiff.js';
 
 function genDiff(filepath1, filepath2, formatName = 'stylish') {
   const readFile1 = fs.readFileSync(filepath1, 'utf-8');
