bf663afa5659c72155a387f0076537ff0588b9fc
Irina <hikarinakano95@gmail.com>
2023-03-10 15:59:27 +0100

    Rewrote gendiff to have changed status

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,7 +4,7 @@ import path from 'path';
 import parse from './parse.js';
 import format from './formatters/index.js';
 
-function computeDiff(data1, data2) {
+/*function computeDiff(data1, data2) {
   if (!(data2 instanceof Object)) {
     return data2;
   }
@@ -29,6 +29,40 @@ function computeDiff(data1, data2) {
       return deleted;
     }
     return added;
+  });
+}*/
+
+function isObj(structure) {
+ return structure instanceof Object
+}
+
+function computeDiff(data1, data2) {
+  if (!(data2 instanceof Object)) {
+    return data2;
+  }
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+  return _.map(keys, (key) => {
+    if (!_.has(data1, key)) {
+      const added = { status: 'added', key, value: computeDiff(data2[key], data2[key]) };
+      return added;
+    }
+    if (!_.has(data2, key)) {
+      return { status: 'deleted', key, value: computeDiff(data1[key], data1[key]) };
+    }
+    if (data1[key] !== data2[key]) {
+      if (isObj(data1[key]) && !isObj(data2[key])) {
+      return { status: 'changed', key, value1: computeDiff(data1[key], data1[key]), value2: data2[key]};
+      }  
+      if (!isObj(data1[key])&& isObj(data2[key])) {
+        return { status: 'changed', key, value1: data1[key], value2: computeDiff(data2[key], data2[key])};
+      }
+      else if (!isObj(data1[key])&& !isObj(data2[key])) {
+        return {status: 'changed', key, value1: data1[key], value2: data2[key]};
+      }
+    }  
+      return { status: 'unchanged', key, value: computeDiff(data1[key], data2[key])};
   });
 }
 
